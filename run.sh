881ca9325b288024b06fcf10f3bdda98c65da9a0
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2020-02-20 17:28:24 +0530

    Add documentation

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 wget --recursive --no-parent -nc -A .html.json.gz https://dumps.wikimedia.org/other/contenttranslation/20200214/
-gunzip -rfk dumps.wikimedia.org/other/contenttranslation
+gunzip -rfkv dumps.wikimedia.org/other/contenttranslation
 python parse.py
