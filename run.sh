38a9040fff513b72198d515261adbe64f903f851
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2021-03-25 10:43:42 +0530

    Update dump path

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-wget --recursive --no-parent -nc -A .html.json.gz https://dumps.wikimedia.org/other/contenttranslation/20200214/
+wget --recursive --no-parent -nc -A .html.json.gz https://dumps.wikimedia.org/other/contenttranslation/20210319/
 gunzip -rfkv dumps.wikimedia.org/other/contenttranslation
 python parse.py
